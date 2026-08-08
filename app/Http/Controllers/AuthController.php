24fbe484946fa456d07c22d51696bc44949d9d80
ankurkashyap001 <antalankur2@gmail.com>
2026-08-09 02:04:47 +0530

    logout api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -81,4 +81,15 @@ class AuthController extends Controller
             'token'   => $token,
         ], 200);
     }
+
+    //logout
+    public function logout(Request $request)
+    {
+        // User ka current access token revoke / delete karein
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -38,6 +38,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/orders/{id}', [OrderController::class, 'show']);
     Route::post('/orders', [OrderController::class, 'store']);
 
+    //logout
+    Route::post('/logout', [AuthController::class, 'logout']);
+
 });
 
 Route::middleware('auth:sanctum')->get('/test-token', function (Request $request) {
